b0dedb275d117d6cab409a3550bd02641336575a
davidnoronha1 <davidnoronha@outlook.in>
2026-04-16 23:56:05 +0530

    remove bad ros2 ws and replace with actual submodules

--- ros2_ws/build/custom_msgs/rosidl_generator_py/custom_msgs/msg/__init__.py
+++
@@ -1,2 +0,0 @@
-from custom_msgs.msg._commands import Commands  # noqa: F401
-from custom_msgs.msg._telemetry import Telemetry  # noqa: F401
--- ros2_ws/install/custom_msgs/share/custom_msgs/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(custom_msgs_IDL_FILES "msg/Commands.idl;msg/Telemetry.idl")
-set(custom_msgs_INTERFACE_FILES "msg/Commands.msg;msg/Telemetry.msg")
